2f60fa8d77823d78a1e35c1a7ce84f2ff02ba341
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-01-11 12:38:31 +0200

    Delete src directory

--- src/application_properties.py
+++
@@ -1,5 +0,0 @@
-
-ng = "AT"
-keywords = ["allTitles", "repSearchFlatListTitlesPage", "RepSearchFlatListTitlesPage", "clickSearchBibRecordsMatches("
-            "clickImportedRecords", "clickPreviewRecords(",
-            "cleanupRecords(SetContentType.AllTitles"]
