c27ff3e03b0983d34b751208ff4485bfbb580b65
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-01-16 22:35:24 +0200

    began discrepancy tracker. successful represents in a string all discrepancy.
    next - must work on way to store everything clearly in files per area. then, to find old and new discrepancies

---
+++ src/application_properties.py
@@ -0,0 +1,10 @@
+
+ng_suffix = "AT"
+tests_dir_name = "tests"
+file_ext = ".java"
+test_base_names_to_ignore = ['TestBase', 'TestBaseAlma', 'None']
+keywords = ["allTitles", "repSearchFlatListTitlesPage", "RepSearchFlatListTitlesPage", "clickSearchBibRecordsMatches("
+            "clickImportedRecords", "clickPreviewRecords(", "cleanupRecords(SetContentType.AllTitles",
+            "createBorrowingRequestFromRepository", "createLendingRequestFromRepository", "createBorrowingRequestFromRepository",
+            "createBorrowingRequestFromRepositoryFromNetwork_FulfNetwork", "createBorrowingRequestFromRepositoryFromNetwork",
+            "createBorrowingRequestFromNetworkRepository"]
---
+++ src/refactor_tests/main_refactor_tests.py
@@ -0,0 +1,41 @@
+import os
+import shutil
+
+from src.refactor_tests import create_playlist_from_files as cp, create_new_files as cnf, sort_tests_to_files as sttf
+import src.application_properties as ap
+import src.gen_files.enums
+from src.gen_files import paths as p
+from src.gen_files.ConsoleHelpers import input_helper as ih, print_functions as pf
+
+
+def main_script():
+    init_params()
+    pf.print_step_separator("Refactor Tests")
+    pf.print_step_separator("Step 1: Enter and Analyze Tests")
+    test_path_list = ih.get_tests_from_input()
+    sttf.public_sort_tests_to_files(ng_path=p.ng_tests_dir_path, non_ng_path=p.non_ng_tests_dir_path,
+                                          test_path_list=test_path_list)
+    pf.print_step_separator("Step 2: Create new NG files (optional)")
+    create_ng_tests_if_prompted(test_path_list=test_path_list)
+    pf.print_step_separator("Step 3: Exporting playlist for non ng tests (to load to Automation Player)")
+    cp.public_create_playlist_from_files(p.output_dir, non_ng_tests_path=p.non_ng_tests_dir_path)
+
+
+def create_ng_tests_if_prompted(test_path_list):
+    cmd = ih.get_input_enum_options("Would you like to create ng directories and files (with a " + ap.ng_suffix + " suffix)?\n"
+                                    + "If the " + ap.ng_suffix + " test already exists, the program will not overwrite it.\n"
+                                    + "Warning - cannot undo creating the files programmatically\n",
+                                    list(src.gen_files.enums.OptionsYesNo))
+    if cmd == src.gen_files.enums.OptionsYesNo.YES.value:
+        cnf.create_ng_versions_of_all_tests(test_path_list)
+    # elif cmd == cons.OptionsYesNo.NO.value:
+
+
+def init_params():
+    global cur_dir, ng_path, non_ng_path
+    shutil.rmtree(p.output_dir)
+    os.makedirs(p.output_dir)
+
+
+if __name__ == '__main__':
+    main_script()
