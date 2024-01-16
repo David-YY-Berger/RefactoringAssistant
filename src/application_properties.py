df0c0e3776b3583eba8109fb4936d22791fa700d
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-01-16 09:39:42 +0200

    finished creating new NG files! now must make runner code. then descrepancy tracker

---
+++ src/application_properties.py
@@ -0,0 +1,8 @@
+
+ng_suffix = "ZZ"
+tests_dir_name = "tests"
+file_ext = ".java"
+test_base_names_to_ignore = ['TestBase', 'TestBaseAlma', 'None']
+keywords = ["allTitles", "repSearchFlatListTitlesPage", "RepSearchFlatListTitlesPage", "clickSearchBibRecordsMatches("
+            "clickImportedRecords", "clickPreviewRecords(",
+            "cleanupRecords(SetContentType.AllTitles"]
---
+++ src/gen_files/input_helper.py
@@ -0,0 +1,88 @@
+
+
+def readable(str):
+    return str.capitalize().replace('_', ' ')
+
+
+def get_input_enum_options(prompt, option_list):
+    print(prompt + " (Enter just the number)")
+    possible_options = []
+    for op in option_list:
+        print(str(op.value) + ". " + readable(op.name))
+        possible_options.append(str(op.value))
+    keep_going = True
+    while keep_going:
+        cmd = input()
+        if not cmd.isnumeric():
+            print("Please enter a number")
+        elif cmd not in possible_options:
+            print("Please only enter one of the given options")
+        else:
+            keep_going = False
+            return int(cmd)
+
+
+def get_input_list(prompt):
+    print(prompt + ". Enter 'end' to finish")
+    input_list = []
+    buf = " "
+    while (buf != "end"):
+        input_list.append(buf)
+        buf = input()
+    input_list = [s for s in input_list if not s.isspace()]
+    print("Processing...")
+    return input_list
+
+
+def get_input_ensure_valid(prompt, boolean_function, error_msg):
+    buf = ''
+    while True:
+        buf = input(prompt)
+        if boolean_function(buf):
+            return buf
+        else:
+            print(error_msg)
+
+
+def get_chosen_items_from_list(init_prompt, enter_list_prompt, option_list):
+    print(init_prompt)
+    select_all = 'Select all'
+    option_list = [select_all] + option_list
+
+    for i, option in enumerate(option_list, start=1):
+        print(f"{i}. {option}")
+    res = set()
+    print(enter_list_prompt + "Separate numbers by whitespace. Enter 'end' to finish: ")
+
+    while True:
+        user_input = input()
+        if user_input.lower() == 'end':
+            break
+        try:
+            # Split the input into a list of integers
+            choices = {int(choice) for choice in user_input.split()}
+            res.update(option_list[choice - 1] for choice in choices if 1 <= choice <= len(option_list))
+        except ValueError:
+            print("Invalid input. Please enter valid numbers separated by whitespace.")
+    if(res.__contains__(select_all)):
+        option_list.remove(select_all)
+        return option_list
+    else:
+        return res
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
