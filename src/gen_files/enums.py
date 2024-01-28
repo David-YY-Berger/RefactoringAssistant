505e05a7ae5c92e3512be0a449182dcd5f856dec
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-01-28 11:40:45 +0200

    added servers, fixed create playlist. still need test list compare

---
+++ src/gen_files/enums.py
@@ -0,0 +1,45 @@
+from enum import Enum, auto
+
+
+def get_name_from_value_enum(val, enum_class):
+    enum_dict = {item.value: item.name for item in enum_class}
+    return enum_dict.get(val, 'could not find enum with value ' + str(val))
+
+
+def readable(str):
+    return str.capitalize().replace('_', ' ')
+
+
+class Servers(Enum):
+    QAC01 = 0
+    SQA_EU01 = auto()
+    SQA_NA01 = auto()
+    COULD_NOT_FIND_SERVER = auto()
+    IGNORE = auto()
+    SQA_EU03 = auto()
+    SQA_NA02 = auto()
+    SQA_EU04 = auto()
+    SQA_EU02 = auto()
+
+
+class OptionsInputTests(Enum):
+    BY_DIRECTORY_PATH = auto()
+    BY_A_LIST_OF_TEST_NAMES = auto()
+    BY_PATH_TO_EXCEL_FILE = auto()
+    BY_PATH_TO_CSV_FILE = auto()
+
+
+class OptionsYesNo(Enum):
+    YES = auto()
+    NO = auto()
+
+
+class MainOptions(Enum):
+    REFACTOR_TESTS = auto()
+    TEST_LIST_COMPARE = auto()
+    RUN_DISCREPANCY_TRACKER = auto()
+    LOGIN_AS_ADMIN = auto()
+    EXIT = auto()
+
+
+
