59ed1295e10790a401127870d58f23007e9cbdb2
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-01-28 19:46:27 +0200

    Finished list compare! ready exe for presentation. Still need setup, then discrepancy tracker. image for exe

---
+++ src/application_properties.py
@@ -0,0 +1,17 @@
+
+ng_suffix = "ZZ"
+tests_dir_name = "tests"
+file_ext = ".java"
+
+excel_row_of_column_header = 1
+excel_column_with_tests_name = "Test"
+
+csv_section_header = "Section Hierarchy"
+csv_column_With_tests_name = "Autotest name"
+
+test_base_names_to_ignore = ["TestBase", "TestBaseAlma", "None"]
+keywords = ["allTitles", "repSearchFlatListTitlesPage", "RepSearchFlatListTitlesPage", "clickSearchBibRecordsMatches("
+            "clickImportedRecords", "clickPreviewRecords(", "cleanupRecords(SetContentType.AllTitles",
+            "createBorrowingRequestFromRepository", "createLendingRequestFromRepository", "createBorrowingRequestFromRepository",
+            "createBorrowingRequestFromRepositoryFromNetwork_FulfNetwork", "createBorrowingRequestFromRepositoryFromNetwork",
+            "createBorrowingRequestFromNetworkRepository"]
