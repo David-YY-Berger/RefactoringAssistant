4e2107f378e63dc15c10af22aa71407aea0c0fcc
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-01-30 15:32:25 +0200

    fixed bugs, refactored. Enabled getting all tests in testngs

---
+++ src/gen_files/paths.py
@@ -0,0 +1,31 @@
+import os
+
+# must change this to relative path! when do sync with intelliJ
+testng_dir_orig = "C:\\urm\\workspace-1.0.0.2-URM\\alma_itest_ux\\src\\test\\resources"
+all_tests_dir = "C:\\urm\\workspace-1.0.0.2-URM\\alma_itest_ux\\src\\test\\java\\tests"
+
+# fake directories:
+# testng_dir_orig = "C:\\RefactoringAssistant\\RefactoringAssistantCode\\test_data\\src\\test\\resources"
+# all_tests_dir = "C:\\RefactoringAssistant\\RefactoringAssistantCode\\test_data\\src\\test\\java\\tests"
+
+cur_dir = os.path.dirname(os.path.abspath(__file__))
+output_dir = os.path.join(os.getenv('LOCALAPPDATA'), 'RefactoringAssistant', 'output')
+temp_dir = os.path.join(os.getenv('LOCALAPPDATA'), 'RefactoringAssistant', 'temp')
+
+ng_tests_dir_path = os.path.join(output_dir, "tests_containing_ng.txt")
+non_ng_tests_dir_path = os.path.join(output_dir, "tests_do_not_contain_ng.txt")
+
+# temp maps creating at runtime from project
+test_name_header = 'test_name'
+testng_header = 'testng_file'
+test_path_header = "test_path"
+temp_dict_test_name_to_path = os.path.join(temp_dir, 'temp_dict_test_name_to_path.csv')
+temp_dict_testng_to_test_name = os.path.join(temp_dir, 'temp_dict_testng_to_test_name.csv')
+# temp map creating from string in file
+testng_server_csv_path = os.path.join(temp_dir, "testng_servers.csv")
+
+
+# fix this:
+application_properties_path = os.path.join(cur_dir, "..", 'application_properties.py')
+
+
---
+++ src/test_list_compare/main_test_list_compare.py
@@ -0,0 +1,39 @@
+from src.gen_files.ConsoleHelpers import input_helper as ih, print_functions as pf
+
+
+def main_script():
+    pf.print_step_separator("Source Compare")
+    print("Enter 2 different lists of test, to see discrepancies between the 2 lists.")
+    pf.print_bold("\nSource 1:")
+    test_names1 = ih.get_test_paths_from_input(get_only_test_names=True)
+    pf.print_bold("\nSource 2:")
+    test_names2 = ih.get_test_paths_from_input(get_only_test_names=True)
+    differences_list = get_dif_str_from_lsts(test_names1, test_names2)
+    pf.print_note("\n\nTests found only in Source 1 (" + str(len(differences_list[0])) + " tests):")
+    pf.print_list_by_row(differences_list[0])
+    pf.print_note("\n\nTests found only in Source 2 (" + str(len(differences_list[1])) + " tests):")
+    pf.print_list_by_row(differences_list[1])
+    pf.print_note("\n\nTests found in both Sources (" + str(len(differences_list[2])) + " tests):")
+    pf.print_list_by_row(differences_list[2])
+    pf.print_note("\nTotal tests analyzed: " + str(sum(len(sublist) for sublist in differences_list)) + ".\n"
+                  + "Only in Source 1: " + str(len(differences_list[0])) + " Only in Source 2: " + str(len(differences_list[1])) + "| In Both: " + str(differences_list[2]))
+
+
+def get_dif_str_from_lsts(test_names1, test_names2):
+    set1 = set(test_names1)
+    set2 = set(test_names2)
+
+    # items that exist only in test_names1
+    only_in_test_names1 = set1 - set2
+    res1 = [item for item in test_names1 if item in only_in_test_names1]
+    # items that exist only in test_names2
+    only_in_test_names2 = set2 - set1
+    res2 = [item for item in test_names2 if item in only_in_test_names2]
+    # items that exist in both
+    res3 = list(set1.intersection(set2))
+
+    return [res1, res2, res3]
+
+
+if __name__ == '__main__':
+    main_script()
