4e45af5a46ce48d4d91c037448d8a265aeae56d9
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-02-14 10:59:11 +0200

    Add files via upload

---
+++ src/application_properties.py
@@ -0,0 +1,28 @@
+
+ng_suffix = "AT"
+tests_dir_name = "tests"
+file_ext = ".java"
+version = '1.4'
+
+excel_row_of_column_header = 1
+excel_column_with_tests_name = "Test"
+excel_column_with_result = "Result"
+excel_result_passed = "Passed"
+excel_result_passed_wo_code_change = "Passed w/o code change"
+# sheets_to_ignore_in_excel = []
+
+
+csv_column_with_section = "Run"
+csv_column_with_test_name = "Autotest name"
+csv_column_with_type = "Type"
+csv_all_column_headers = [csv_column_with_test_name, csv_column_with_section, csv_column_with_type]
+csv_type_value_automated = "Automated"
+csv_type_value_manual = "Manual"
+
+test_base_names_to_ignore = ["TestBase", "TestBaseAlma", "None"]
+keywords = ["allTitles", "repSearchFlatListTitlesPage", "RepSearchFlatListTitlesPage", "clickSearchBibRecordsMatches("
+            "clickImportedRecords", "clickPreviewRecords(", "cleanupRecords(SetContentType.AllTitles",
+            "createBorrowingRequestFromRepository", "createLendingRequestFromRepository", "createBorrowingRequestFromRepository",
+            "createBorrowingRequestFromRepositoryFromNetwork_FulfNetwork", "createBorrowingRequestFromRepositoryFromNetwork",
+            "createBorrowingRequestFromNetworkRepository", "openBibInNMDEditorReturnsMARCFieldsPage",
+            "openBibInNMDEditor"]
---
+++ src/gen_files/stats_for_metrics.py
@@ -0,0 +1,117 @@
+from enum import Enum, auto
+
+import pandas as pd
+from datetime import datetime
+import os
+from src.gen_files import paths as p
+
+
+# Define the field names
+version_field = 'version'
+user_name_field = 'user_name'
+date_field = 'date'
+time_field = 'time'
+product_field = 'product'
+is_success_field = 'is_success'
+num_files_created_field = 'num_files_created'
+missing_files_found_field = 'missing_files_found'
+files_added_to_testng_field = 'files_added_to_testng'
+error_msg_field = 'error_msg'
+tests_analyzed_field = 'test_analyzed'
+ng_files_found_field = 'ng_files_found'
+non_ng_files_found_field = 'non_ng_files_found'
+total_files_with_descrepancies_field = 'total_files_with_descrepancies'
+total_files_without_descrepancies_field = 'total_files_without_descrepancies'
+passed_tests_with_no_ng_path_field = 'passed_tests_with_no_ng_path'
+passed_tests_found_with_no_change_field = 'passed_tests_found_with_no_change'
+ng_files_overwritten_field = 'ng_files_overwritten'
+files_with_new_descrepencies_field = 'files_with_new_descrepencies'
+
+
+
+flag_ignore_david = True
+
+
+class product_names(Enum):
+    Refactor_Tests = auto()
+    Test_List_Compare = auto()
+    Discrepancy_Tracker = auto()
+
+
+# Function to append a row to the CSV file
+# BE CAREFUL WITH THE ORDER OF EVERYTHING!!!!
+def append_to_stats(version=None, user_name=None, date=None, time=None,
+                    product=None, is_success=None, num_files_created=None,
+                    missing_files_found=None, files_added_to_testng=None,
+                    error_msg=None, tests_analyzed=None, ng_files_found=None,
+                    non_ng_files_found=None, total_files_with_descrepancies=None,
+                    total_files_without_descrepancies=None,
+                    passed_tests_with_no_ng_path=None,
+                    passed_tests_found_with_no_change=None,
+                    ng_files_overwritten=None,
+                    files_with_new_descrepencies=None):
+
+    if user_name == 'davidbe' and flag_ignore_david:
+        return
+    file_path = p.path_for_stats
+    header = [version_field, user_name_field, date_field, time_field,
+              product_field, is_success_field, num_files_created_field,
+              missing_files_found_field, files_added_to_testng_field,
+              error_msg_field, tests_analyzed_field, ng_files_found_field,
+              non_ng_files_found_field, total_files_with_descrepancies_field,
+              total_files_without_descrepancies_field,
+              passed_tests_with_no_ng_path_field,
+              passed_tests_found_with_no_change_field,
+              ng_files_overwritten_field,
+              files_with_new_descrepencies_field]
+
+    try:
+        # Create a dictionary with the data
+        data = {
+            version_field: [version],
+            user_name_field: [user_name],
+            date_field: [date],
+            time_field: [time],
+            product_field: [product],
+            is_success_field: [is_success],
+            num_files_created_field: [num_files_created],
+            missing_files_found_field: [missing_files_found],
+            files_added_to_testng_field: [files_added_to_testng],
+            error_msg_field: [error_msg],
+            tests_analyzed_field: [tests_analyzed],
+            ng_files_found_field: [ng_files_found],
+            non_ng_files_found_field: [non_ng_files_found],
+            total_files_with_descrepancies_field: [total_files_with_descrepancies],
+            total_files_without_descrepancies_field: [total_files_without_descrepancies],
+            passed_tests_with_no_ng_path_field: [passed_tests_with_no_ng_path],
+            passed_tests_found_with_no_change_field: [passed_tests_found_with_no_change],
+            ng_files_overwritten_field: [ng_files_overwritten],
+            files_with_new_descrepencies_field: [files_with_new_descrepencies]
+        }
+
+        # Create a DataFrame
+        df = pd.DataFrame(data)
+
+        # Check if the CSV file already exists
+        if os.path.exists(file_path):
+            # If it exists, append the DataFrame to the CSV file without the header
+            df.to_csv(file_path, mode='a', header=False, index=False)
+        else:
+            # If it doesn't exist, create a new file with the header
+            df.to_csv(file_path, index=False)
+
+    except PermissionError:
+        # If there's a permission error, create a new file in the same directory with a different name
+        new_file_path = os.path.join(p.dir_for_stats, f'{user_name}_temp.csv')
+
+        # Check if the user-specific file already exists
+        if os.path.exists(new_file_path):
+            # If it exists, append the data to the existing file without the header
+            df.to_csv(new_file_path, mode='a', header=False, index=False)
+        else:
+            # If it doesn't exist, create a new file with the header
+            df_header = pd.DataFrame(columns=header)
+            df_header.to_csv(new_file_path, index=False)
+
+            # Append the data to the new file
+            df.to_csv(new_file_path, mode='a', header=False, index=False)
