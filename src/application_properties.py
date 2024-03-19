093ad45c35595781e71da48ee61ae9f3cce5a9ca
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-03-19 08:09:45 +0200

    Significantly optimised DiscrepancyTracker. Added external JSON config file

---
+++ src/application_properties.py
@@ -0,0 +1,19 @@
+ng_suffix = "to be initialized during setup"
+tests_dir_name = "to be initialized during setup"
+file_ext = "to be initialized during setup"
+version = 'to be initialized during setup'
+
+excel_row_of_column_header = -1
+excel_column_with_tests_name = "to be initialized during setup"
+excel_column_with_result = "to be initialized during setup"
+excel_result_passed = "to be initialized during setup"
+excel_result_passed_wo_code_change = "to be initialized during setup"
+
+
+csv_column_with_section = "to be initialized during setup"
+csv_column_with_test_name = "to be initialized during setup"
+csv_column_with_type = "to be initialized during setup"
+csv_all_column_headers = ["to be initialized during setup"]
+
+test_base_names_to_ignore = ["to be initialized during setup"]
+keywords = ["to be initialized during setup"]
