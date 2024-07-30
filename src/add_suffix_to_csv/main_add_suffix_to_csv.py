0dfdd9fb99a7633fc44a0f48b7859734289bf035
David Berger <91850832+David-YY-Berger@users.noreply.github.com>
2024-07-30 10:09:29 +0300

    final updates

---
+++ src/add_suffix_to_csv/main_add_suffix_to_csv.py
@@ -0,0 +1,73 @@
+import os
+import pandas as pd
+from src.gen_files.ConsoleHelpers import print_functions as pf, input_helper as ih
+from src.gen_files import file_utils as fu, stats_for_metrics as sfm, gen_scripts as gs, paths as p
+from src import application_properties as ap
+import os
+import pandas as pd
+
+notes = ["This script adds a suffix to each test name in given csv files",
+         "It will run on all csv's in the given directory",
+         "If there are non CSV files (including other directories) in this directory, it will not read them",
+         "It will read the column headers from the first csv, and assume that the chosen column header exists in every other csv",
+         "The output can be found in " + p.output_dir]
+
+
+def main_script():
+    pf.print_step_separator("Add Suffix to Test name in CSV file")
+    gs.clear_create_dir(p.add_suffix_output_dir)
+    pf.print_note_list(notes, "How this works:")
+    suffix = input("Enter the Suffix you would like to add to the test names (for example - " + ap.ng_suffix + ")\n")
+    src_dir = ih.get_input_ensure_valid("Enter the path to the directory containing the csv files: ", os.path.isdir,
+                           "Please ensure that path leads to a valid directory")
+
+    total_rows_edited = add_suffix_to_autotest_names(src_dir, suffix)
+
+    print(" ")
+    pf.print_note("Edited a total of " + str(total_rows_edited) + " in all csv files")
+
+    sfm.append_to_stats(version=ap.version, user_name=gs.get_cur_user_name(), date=gs.get_date(), time=gs.get_time(),
+                        product=sfm.product_names.Add_Suffix_To_CSV.name, is_success=True,
+                        tests_analyzed= total_rows_edited)
+
+
+def add_suffix_to_autotest_names(input_directory_path, suffix, ):
+    # List all CSV files in the directory
+    csv_files = [file for file in os.listdir(input_directory_path) if file.endswith('.csv')]
+    if len(csv_files) == 0:
+        pf.print_warning("Did not find any valid csv files in " + input_directory_path)
+        return 0
+
+    dir_name = os.path.basename(input_directory_path)
+    output_dir = os.path.join(p.add_suffix_output_dir, dir_name)
+    gs.clear_create_dir(output_dir)
+
+    first_file = os.path.join(input_directory_path, csv_files[0])
+    column_header = ih.get_one_item_from_list('Discovered the following column header names',
+                                              'Which column to add the ' + suffix + ' to?',
+                                              pd.read_csv(first_file).columns.to_list())
+
+    total_rows_edited = 0
+
+    pf.print_note("Adding '" + suffix + "' to every row under '" + column_header +"'")
+    # Loop through each CSV file
+    for file_name in csv_files:
+        input_file_path = os.path.join(input_directory_path, file_name)
+
+        # Read CSV file into a DataFrame
+        df = pd.read_csv(input_file_path)
+        original_non_nan_count = df[column_header].notna().sum()
+        # Add suffix to the end of every non-NaN value in the chosen column, before the '.'
+        df[column_header] = df[column_header].apply(lambda x: x.replace('.', f'{suffix}.') if pd.notna(x) and '.' in x else (f'{x}{suffix}' if pd.notna(x) else x))
+
+        # Write the modified DataFrame back to the CSV file
+        output_file_path = os.path.join(output_dir, suffix + '_Version_' + file_name)
+        df.to_csv(output_file_path, index=False)
+        pf.print_note("Created: " + output_file_path)
+        total_rows_edited += original_non_nan_count
+
+    return total_rows_edited
+
+
+if __name__ == '__main__':
+    main_script()
---
+++ src/gen_files/csv_pandas_ops.py
@@ -0,0 +1,149 @@
+import os.path
+
+import pandas as pd
+from src.gen_files import paths as p, gen_scripts as gs, constants as cons
+from src.gen_files.ConsoleHelpers import print_functions as pf
+from src.Classes.test_class import Test
+import warnings
+
+cur_suffix = '_cur'
+old_suffix = '_old'
+
+
+def create_csv_from_test_obj_list(test_obj_list, path):
+    df = pd.DataFrame([vars(obj) for obj in test_obj_list])
+    df.to_csv(path, index=False)
+
+
+def create_test_obj_list_from_csv(csv_path, from_cur_suffix = False):
+    # Define header names based on the suffix condition
+    test_name_header = Test.TEST_NAME_FIELD # never changes
+    test_path_header = Test.TEST_PATH_FIELD + cur_suffix if from_cur_suffix else Test.TEST_PATH_FIELD
+    test_section_header = Test.TEST_SECTION_FIELD + cur_suffix if from_cur_suffix else Test.TEST_SECTION_FIELD
+    test_result_header = Test.TEST_RESULT_FIELD + cur_suffix if from_cur_suffix else Test.TEST_RESULT_FIELD
+    testng_file_name_header = Test.TESTNG_FILE_NAME_FIELD + cur_suffix if from_cur_suffix else Test.TESTNG_FILE_NAME_FIELD
+    test_path_ng_header = Test.TEST_PATH_NG_FIELD + cur_suffix if from_cur_suffix else Test.TEST_PATH_NG_FIELD
+    discrepancy_header = Test.DISCREPANCY_FIELD + cur_suffix if from_cur_suffix else Test.DISCREPANCY_FIELD
+    # for non ng content - we take the old suffix!!!
+    non_ng_content_header = Test.NON_NG_CONTENT + old_suffix if from_cur_suffix else Test.NON_NG_CONTENT
+
+    df = pd.read_csv(csv_path)
+    test_objects = []
+
+    # Iterate over rows in the DataFrame
+    for index, row in df.iterrows():
+        # Extract values from the row
+        test_name = row[test_name_header] if test_name_header in df.columns else 'Error didnt find test name column in csv!'
+        test_path = row[test_path_header] if test_path_header in df.columns else cons.empty_char
+        test_section = row[test_section_header] if test_section_header in df.columns else cons.empty_char
+        test_result = row[test_result_header] if test_result_header in df.columns else cons.empty_char
+        testng_file_name = row[testng_file_name_header] if testng_file_name_header in df.columns else cons.empty_char
+        test_path_ng = row[test_path_ng_header] if test_path_ng_header in df.columns else cons.empty_char
+        discrepancy = row[discrepancy_header] if discrepancy_header in df.columns else cons.empty_char
+        non_ng_content = row[non_ng_content_header] if non_ng_content_header in df.columns else cons.empty_char
+
+        # Create a Test object and append it to the list
+        test_objects.append(Test(test_name, test_path=test_path,test_section= test_section, test_result=test_result,
+                                 testng_file_name=testng_file_name, test_path_ng=test_path_ng, discrepancy=discrepancy,
+                                 non_ng_content=non_ng_content))
+
+    return test_objects
+
+
+
+def write_paths_to_test_objs(test_obj_csv_path, test_to_path_csv_path, output_path ='', ignore_warning=False):
+    # Read the CSV files
+
+    test_to_paths_df = pd.read_csv(test_to_path_csv_path)
+    duplic_in_test_to_path = find_non_unique_values(test_to_path_csv_path, p.test_name_header)
+    if len(duplic_in_test_to_path) > 0:
+        if not ignore_warning:
+            pf.print_warning("Found duplicates test names in project (" + test_to_path_csv_path + '):\n' +
+                             gs.open_list_as_string(duplic_in_test_to_path, '\n')
+                             + '\n ignoring the duplicates..')
+        test_to_paths_df = test_to_paths_df.drop_duplicates(subset=p.test_name_header, keep=False)
+
+    test_objs_df = pd.read_csv(test_obj_csv_path)
+    duplic_in_test_obj = find_non_unique_values(test_obj_csv_path, p.test_name_header)
+    if len(duplic_in_test_obj) > 0:
+        pf.print_warning("Found duplicates in " + test_obj_csv_path + ':\n' +
+                         gs.open_list_as_string(duplic_in_test_obj, '\n')
+                         + '\n ignoring the duplicates..')
+        test_objs_df = test_objs_df.drop_duplicates(subset='test_names', keep=False)
+
+    # Merge the DataFrames on the 'test_name' column
+    merged_df = pd.merge(test_objs_df,test_to_paths_df[[p.test_name_header, p.test_path_header]], on=p.test_name_header, how='left')
+    # Update 'test_path' column with non-null values from 'test_path_y' column
+    merged_df[p.test_path_header] = merged_df[p.test_path_header + '_y'].combine_first(merged_df[p.test_path_header + '_x'])
+    # Drop unnecessary columns
+    merged_df.drop([p.test_path_header + '_x', p.test_path_header + '_y'], axis=1, inplace=True)
+    # Write the updated DataFrame back to CSV
+    merged_df.to_csv(output_path, index=False)
+
+
+def remove_tests_with_no_path(orig_csv_path, output_csv_path):
+    # Read the CSV file into a DataFrame
+    df = pd.read_csv(orig_csv_path)
+
+    # Step 1: Get a list of rows (just the test_name) that do not have a test_path or have '-'
+    test_names_without_path = df[df['test_path'].isin([None, '-'])][Test.TEST_NAME_FIELD].tolist()
+
+    # Step 2: Remove rows without test_path or containing '-' from the DataFrame
+    df = df.dropna(subset=['test_path'])
+    df = df[df['test_path'] != '-']
+
+    # Save the updated DataFrame back to a CSV file
+    df.to_csv(output_csv_path, index=False)
+
+    pf.print_warning("\nCould not find test_path for these tests (" + str(len(test_names_without_path)) + "):\n" +
+                     gs.open_list_as_string(test_names_without_path, "\n"))
+
+    pf.print_warning("Excluded " + str(len(test_names_without_path)) + " tests (without paths) from the analysis"
+                   "\nPossible reasons why could not find paths:\n"
+                   "\t- Typo in the test name entered (search is Case Sensitive!)\n"
+                   "\t- Found more than one file with the name entered (program ignores duplicated)\n"
+                   "Please double check the test names entered. You can also try by entering a directory.")
+    return df
+
+
+def find_non_unique_values(csv_path, col_header):
+    # Read the CSV file into a Pandas DataFrame
+    df = pd.read_csv(csv_path)
+
+    # Find and return values that are not unique in the specified column
+    non_unique_values = df[df.duplicated(col_header, keep=False)][col_header].tolist()
+    return list(set(non_unique_values))
+
+
+def write_new_discrepancies_to_csvs(old_csv_path, cur_csv_path, old_test_new_discreps_path, new_tests_new_discreps_path):
+    warnings.filterwarnings("ignore", category=UserWarning)
+
+    old_df = pd.read_csv(old_csv_path)
+    cur_df = pd.read_csv(cur_csv_path)
+
+    old_tests_df = cur_df[cur_df[Test.TEST_NAME_FIELD].isin(old_df[Test.TEST_NAME_FIELD])]
+    new_tests_w_new_discrep_df = cur_df[~cur_df[Test.TEST_NAME_FIELD].isin(old_df[Test.TEST_NAME_FIELD])]
+
+    # Find discrepancies in old tests that are different from old_df
+    merged_df = pd.merge(old_tests_df, old_df, on=Test.TEST_NAME_FIELD, suffixes=(cur_suffix, old_suffix))
+    old_tests_new_discrep_df = merged_df[merged_df[Test.DISCREPANCY_FIELD + cur_suffix] != merged_df[Test.DISCREPANCY_FIELD + old_suffix]]
+
+    # Write to CSV files
+    old_tests_new_discrep_df.to_csv(old_test_new_discreps_path, index=False)
+    new_tests_w_new_discrep_df.to_csv(new_tests_new_discreps_path, index=False)
+    num_new_discrep_with_old_tests = len(old_tests_new_discrep_df)
+    num_new_discrep_with_new_tests = len(new_tests_w_new_discrep_df)
+    total_new_discreps = num_new_discrep_with_old_tests + num_new_discrep_with_new_tests
+
+    return [total_new_discreps, num_new_discrep_with_old_tests, num_new_discrep_with_new_tests]
+
+
+def remove_rows_with_invalid_val_in_col_df(orig_df, column, func_validate):
+    # Apply the validation function to the specified column
+    valid_mask = orig_df[column].apply(func_validate)
+
+    # Filter the DataFrame using the mask
+    new_df = orig_df[valid_mask]
+
+    return new_df
+
